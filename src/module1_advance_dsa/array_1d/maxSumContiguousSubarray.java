047d8701c626ca74adc11f336caf3988ff45d9fc
Abhishek Diwate (adiwate) <adiwate@cisco.com>
2023-05-23 10:10:10 +0530

    module 1 array_1d assignment

---
+++ src/module1_advance_dsa/array_1d/maxSumContiguousSubarray.java
@@ -0,0 +1,81 @@
+package module1_advance_dsa.array_1d;
+
+import java.util.List;
+
+/**
+ Find the contiguous non-empty subarray within an array, A of length N, with the largest sum.
+
+ Problem Constraints
+ 1 <= N <= 1e6
+ -1000 <= A[i] <= 1000
+
+ Input Format
+ The first and the only argument contains an integer array, A.
+
+ Output Format
+ Return an integer representing the maximum possible sum of the contiguous subarray.
+
+ Example Input
+ Input 1:
+ A = [1, 2, 3, 4, -10]
+ Input 2:
+ A = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+
+ Example Output
+ Output 1:
+ 10
+ Output 2:
+ 6
+
+ Example Explanation  1:
+
+ The subarray [1, 2, 3, 4] has the maximum possible sum of 10.
+ Explanation 2:
+
+ The subarray [4,-1,2,1] has the maximum possible sum of 6.
+ */
+
+public class maxSumContiguousSubarray {
+
+    /**
+     Brute force approach
+     TC: O(n2)
+     SC: O(1)
+     */
+    public int maxSubArray(final List<Integer> A) {
+        int maxAns = Integer.MIN_VALUE;
+        int n = A.size();
+        for(int i=0; i<n; i++)
+        {
+            int sum = 0;
+            for(int j=i; j<n; j++)
+            {
+                sum += A.get(j);
+                maxAns = Math.max(sum, maxAns);
+
+            }
+        }
+        return maxAns;
+    }
+
+
+    /**
+     Optimised approach
+     TC: O(n)
+     SC: O(1)
+     */
+    public int maxSubArray2(final List<Integer> A) {
+        int maxAns = Integer.MIN_VALUE;
+        int n = A.size();
+        int sum = 0;
+        for (Integer integer : A) {
+            sum += integer;
+            maxAns = Math.max(sum, maxAns);
+
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return maxAns;
+    }
+}
---
+++ src/module1_advance_dsa/array_1d/rainWaterTrapped.java
@@ -0,0 +1,118 @@
+package module1_advance_dsa.array_1d;
+
+import java.util.List;
+
+/**
+ Given a vector A of non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining.
+
+ Problem Constraints
+ 1 <= |A| <= 100000
+
+ Input Format
+ First and only argument is the vector A
+
+ Output Format
+ Return one integer, the answer to the question
+
+ Example Input
+ Input 1:
+ A = [0, 1, 0, 2]
+ Input 2:
+ A = [1, 2]
+
+ Example Output
+ Output 1:
+ 1
+ Output 2:
+ 0
+
+ Example Explanation  1:
+ 1 unit is trapped on top of the 3rd element.
+ Explanation 2:
+ No water is trapped.
+ */
+
+public class rainWaterTrapped {
+
+    /**
+     Approach by calculating left Max and right Max array
+     TC: 0(N)
+     SC: O(N)
+     */
+    public int trap(final List<Integer> A) {
+
+        int n = A.size();
+        int [] left = new int[n];
+        int [] right = new int[n];
+
+        left[0] = A.get(0);
+        right[n-1] = A.get(n-1);
+
+        for(int i=1; i<n; i++)
+        {
+            left[i] = Math.max(left[i-1], A.get(i));
+        }
+
+        for(int i=n-2; i>=0; i--)
+        {
+            right[i] = Math.max(right[i+1], A.get(i));
+        }
+
+        int ans = 0;
+        for(int i=0; i<n; i++)
+        {
+            int min = Math.min(left[i], right[i]);
+            ans += (min - A.get(i));
+
+        }
+
+        return ans;
+    }
+
+    /**
+    Optimised two pointer approach
+     SC: O(N)
+     TC: O(1)
+     */
+    public int trap2(final List<Integer> a) {
+
+        int n = a.size();
+
+        int leftMax = 0;
+        int rightMax = 0;
+
+        int lp = 0;
+        int rp = n-1;
+
+        int ans = 0;
+
+        while(lp < rp)
+        {
+            if(a.get(lp) <= a.get(rp))
+            {
+                if(a.get(lp) > leftMax)
+                {
+                    leftMax = a.get(lp);
+                }
+                else
+                {
+                    ans += (leftMax - a.get(lp));
+                }
+                lp++;
+            }
+            else{
+                if(a.get(rp) > rightMax)
+                {
+                    rightMax = a.get(rp);
+                }
+                else
+                {
+                    ans += (rightMax - a.get(rp));
+                }
+                rp --;
+            }
+        }
+        return ans;
+
+    }
+}
